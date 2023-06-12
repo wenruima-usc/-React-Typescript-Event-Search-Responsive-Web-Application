2b6edfc0992cdd8d2ba66007f3dd9c2f97acc81b
Wenrui Ma <wenruima@usc.edu>
2023-06-11 18:41:52 -0700

    update

--- event-search-app/src/components/Detail.tsx
+++ event-search-app/src/components/Detail.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { EventDetail } from './ResultTable';
 import {Button} from "react-bootstrap";
 import './Detail.css';
+import {FaChevronLeft,FaRegHeart} from 'react-icons/fa'
+import {Tabs,Tab} from "@mui/material"
+
 interface DetailProps{
     eventDetailProps: EventDetail
 }
@@ -12,10 +15,24 @@ const Detail: React.FC<DetailProps>= ({eventDetailProps}) =>{
         <div className='mt-5'>
             <div className='col-md-8 mx-auto mt-5 detailTabs p-0' >
                 <div>
-                    <Button type="button" className='btn-sm back mt-3'>
-                    <i className="material-icons md-12 md-light align-items-center">chevron_left</i> <span className="back">Back</span>
-                    </Button>
+                    <button type="button" className='btn btn-sm back mt-3'>
+                    <FaChevronLeft></FaChevronLeft><span className="back">Back</span>
+                    </button>
+                    <div className="card-title pt-4">
+                        <p>
+                           <span style={{marginRight:'5px'}}>{eventDetailProps.name}</span>
+    
+                            <button type="button" className='btn btn-sm favorite p-0' >
+                                <FaRegHeart style={{color:'black'}}></FaRegHeart>
+                            </button>
+                        </p>
+                    </div>
                 </div>
+               <Tabs indicatorColor="primary" textColor="primary">
+                    <Tab label="Tab 1"></Tab>
+                    <Tab label="Tab 2"></Tab>
+                    <Tab label="Tab 3"></Tab>
+                </Tabs> 
             </div>
         </div>
     );
